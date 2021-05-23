d8729a06050637305441978b4846b899d473c1c4
NadyLuna <nadyedja@gmail.com>
2021-05-23 17:31:08 -0300

    import de express e router

--- api.js
+++ api.js
@@ -1,4 +1,8 @@
-const express = require('express')
+const express = require('express');
+const routes = require('./routes/routes')
 const app = express()
+
+app.use(express.json());
+app.use(routes)
 
 app.listen(3000, () => console.log ("API RODANDO"))
